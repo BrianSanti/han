4eb012d6f7de2e15e463701feb03034c263cfe48
U. Mariano Gálvez <alumno@labs.academico>
2019-04-30 12:03:36 -0600

    Hanoi en objetos

---
+++ Han2/src/Pila.java
@@ -0,0 +1,53 @@
+public class Pila {
+    
+    
+    private int ContNodo = 0;
+    private Nodo Cabeza;
+
+    public Nodo getCabeza() {
+        return Cabeza;
+    }
+
+    public int getContNodo(){  
+    return ContNodo;
+    
+    }
+    
+    public void Push(Nodo N){
+    
+    ContNodo++;
+    if(Cabeza ==null){
+    Cabeza= N;
+    }else {
+    N.setAbajo(Cabeza);
+    Cabeza.setArriba(N);
+    Cabeza = N;
+    }
+    
+    }
+    
+    public void Pop(){
+     
+        if(ContNodo >0){
+        ContNodo--;
+        Cabeza = Cabeza.getAbajo();
+        
+        
+        }
+    
+    
+    }
+    
+
+public String Peek(){
+return Cabeza.getDato();
+}
+
+
+
+
+
+//7:17 segundo video
+
+    
+}
